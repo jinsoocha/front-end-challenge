94424f6fd1cadc07d752a209e0c52c09043b0d0e
Jinsoo Cha <jinscha@indiana.edu>
2016-08-28 20:53:35 -0700

    clean up the code and include loader and transition

--- script.js
+++ script.js
@@ -1,81 +1,62 @@
 
-function domobj(){
-  var self        =this;
+// Helper functions //
 
-  self.getProducts = function() {
-    return $.getJSON('data.json');
-  };
+var getProducts = function() {
+  return $.getJSON('data.json');
+};
 
-  self.getTemplate = function() {
-    return $.get('product-template.html');
-  };
+var getTemplate = function() {
+  return $.get('product-template.html');
+};
 
-  self.createView = function(products, template) {
-    var productsHtml='';
-    for(i=0; i<products.length ; i++){
-      var product = new productobj(products[i], i);
-      var productHtml = template.replace('{image}', product.photo).replace('{descriptionText}', product.description).replace('{title}', product.title).replace('{tagline}', product.tagline).replace('{url}', product.url);
-      if (i % 3 == 0 ){  productsHtml += "<div class='row'>"; console.log("START") }
-      productsHtml += productHtml;
-      if ((i % 3 == 2) || i == (products.length-1) ){productsHtml += "</div>";console.log("FINISH")}
-    }
-    $("#content").append(productsHtml);
-  };
+var createView = function(products, template) {
+  var view = '';
+  products.forEach(function(item) {
+    product = new Productobj(item);
+    var productView = template
+      .replace('{image}', product.photo)
+      .replace('{description}', product.description)
+      .replace('{title}', product.title)
+      .replace('{tagline}', product.tagline)
+      .replace('{url}', product.url);
+    view += productView;     
+  });
+  $("#content").append(view);
+};
 
-  self.render = function(){
-    $.when(self.getProducts(), self.getTemplate()).done(function(products, template){
-      products = products[0].sales;
-      template = template[0];
-      self.createView(products, template);   
-      self.addEventhandlers();
-    });
-  };
-  
-  self.addEventhandlers = function(){
-    var showDescription = function() {
-      $(this).find(".descriptionText").css({
-        display: "block",
-        zIndex: 1000000,
-        color: "white",
-        backgroundColor: "rgba(0, 0, 0, 0.6)"
-      });
-    };
+// Event handlers //
 
-    var hideDescription = function() {
-      $(this).find(".descriptionText").css({
-        display: "none"
-      });
-    };
+var addEventhandlers = function() {
+  $('.remove').click(removeItem).bind(this);
+};
 
-    var changeCursor = function() {
-      $(this).css({
-        cursor: "pointer"
-      });
-    };
+var removeItem = function(e) {
+  e.preventDefault();
+  var target = $(this).closest('.product-container');
+  target.hide('slow', function() { target.remove(); });
+};
 
-    var removeItem = function(e) {
-      e.preventDefault();
-      $(this).closest('.item').remove();
-    };
+// Product class //
 
-    $('.product-container').hover(showDescription, hideDescription).bind(this);
-    $('.remove').hover(changeCursor).bind(this);
-    $('.remove').click(removeItem).bind(this);
-  }
-}
-
-function productobj(product, i){
+function Productobj(product, i) {
   var self          = this;
-  self.description  = product.description
-  self.photo        = product.photos.medium_half
-  self.title        = product.name
-  self.tagline      = product.tagline
-  self.url          = product.url
-  self.htmlview     = ""
-  self.index        = i
-}
+  self.description  = product.description;
+  self.photo        = product.photos.medium_half;
+  self.title        = product.name;
+  self.tagline      = product.tagline;
+  self.url          = product.url;
+  self.index        = i;
+};
 
+// Initialize //
 
-var page=new domobj();
-page.render();
+var render = function() {
+  $.when(getProducts(), getTemplate()).done(function(products, template){
+    products = products[0].sales;
+    template = template[0];
+    createView(products, template);   
+    addEventhandlers();
+  });
+};
 
+render();
